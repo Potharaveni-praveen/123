a9b19eb580891e91c56ddef215ff22de754e591d
praveen-at-401865111165 <BgzquSJuRu7DKPHF39hCkMB/FoaDKuLzBTtEoa7iVlo=>
2021-11-05 19:05:46 +0530

    m

---
+++ script.groovy
@@ -0,0 +1,35 @@
+pipeline{
+    agent any
+    tools{
+        maven 'maven'
+    }       
+    stages{
+        stage("build jar") {
+            steps{
+                script{
+                    echo "building application"
+                    sh 'mvn package'
+                }
+            }
+        }
+        stage("build image") {
+            steps{
+                script{
+                    echo "building docker image"
+                    withCredentials([usernamepassword(credentialsid: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+                       sh 'docker build -f Dockerfile.dockerfile -t 8978677272/praveen:pra-2.0 .'
+                       sh 'echo $PASS | docker login -u $USERNAME --password-stdin'
+                       sh 'docker push 8978677272/praveen:pra-2.0'
+                    }    
+                }
+            }
+        }
+        stage("deploy"){
+            steps{
+                script{
+                    echo "deploying application"
+                }
+            }
+        }
+    }
+}
